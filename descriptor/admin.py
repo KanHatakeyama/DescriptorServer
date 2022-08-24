cd11c56f4646d6c44bbfb7fef3bbb3ff7f66eed4
KanHatakeyama <yaj_k@msn.com>
2022-08-24 18:28:54 +0900

    view

--- descriptor/admin.py
+++ descriptor/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+class MoleculeAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(models.Molecule, MoleculeAdmin)
--- descriptor/models.py
+++ descriptor/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Molecule(models.Model):
+    SMILES = models.CharField(max_length=1500)
+    RDKit_desc = models.TextField(null=True, blank=True, max_length=10**5)
+
+    def __str__(self) -> str:
+        return self.SMILES
